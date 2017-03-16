517b20ac3496fda18d4f3580041ce4dfb2fc669d
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-16 17:16:43 +0700

    contoh fungsi biasa

---
+++ fungsi-biasa.php
@@ -0,0 +1,15 @@
+<?php
+
+function alamat()
+{
+  return "Kemang, Jakarta Selatan";
+}
+
+function nama($rumah)
+{
+  echo "Namaku Dhimas Ganteng, tinggal di daerah $rumah\n";
+}
+
+nama(alamat());
+
+?>
